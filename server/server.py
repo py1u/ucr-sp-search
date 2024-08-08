b06a73b089a2ea1134264cba0a5ad037a67aa16d
py1u.dev <py1u.dev@gmail.com>
2024-08-07 17:09:40 -0700

    test: home page route

--- server/server.py
+++ server/server.py
@@ -1,18 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/api/home", methods=['GET'])
-def return_home():
-    return jsonify({'message': "Hello World!"})
+@app.route("/")
+def main():
+    return '<p>hello world!</p>'
 
-if __name__ == "__main__":
-    app.run(debug=True)
-   
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
