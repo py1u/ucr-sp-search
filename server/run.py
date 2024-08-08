e4e2ed182c3915c16bbb013b326b2cf240338f54
py1u.dev <py1u.dev@gmail.com>
2024-08-08 00:47:17 -0700

    update: add run module imports to allow flask run to start dev env locally

--- server/run.py
+++ server/run.py
@@ -1,7 +1,7 @@
-# from app import create_app
+from app import create_app
 
 
-#flask_app = create_app()
+flask_app = create_app()
 
-# if __name__ == '__main__':
-#     flask_app.run(debug=True)
+if __name__ == '__main__':
+    flask_app.run(debug=True)
