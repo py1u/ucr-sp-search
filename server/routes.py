1e2ca19931492078626df52daa77db687d2b4f00
py1u.dev <py1u.dev@gmail.com>
2024-08-08 00:39:36 -0700

    update: add root URL to test db query for all courses with display to web page

--- server/routes.py
+++ server/routes.py
@@ -6,5 +6,8 @@ from models import Course
 # avoiding import circulation 
 def register_routes(app,db):
     
-    
+    @app.route('/')
+    def my_db():
+        course = Course.query.all()
+        return str(course)
         
