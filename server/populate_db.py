392c5bb99058691e1e5a61ef07fc0ce32c62dff2
py1u.dev <py1u.dev@gmail.com>
2024-08-08 16:12:57 -0700

    update: python function to populate the db with the clean csv

--- server/populate_db.py
+++ server/populate_db.py
@@ -5,4 +5,30 @@ from models import Course
 app = create_app()
 
 
+# populate the instance/testdb.db
+def populate_database():
+    
+    with open('course.csv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
 
+        for row in reader:
+            course = Course(
+                cid=row['course_id'],
+                level=row['level'],
+                college=row['college_desc'],
+                dept=row['department_desc'],
+                course_title=row['title_long_desc'],
+                course_desc=row['course_text_narrative'],
+                sgd=row['sgd']  
+            )
+
+            db.session.add(course)
+            
+           
+            db.session.commit()
+    
+    
+
+if __name__ == '__main__':
+    with app.app_context():
+        populate_database()
