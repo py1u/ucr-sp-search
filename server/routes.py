1c009a18ba422ff4c92933409b7b0bec07396dc4
py1u.dev <py1u.dev@gmail.com>
2024-08-08 23:44:35 -0700

    test: randomized query using sqlalchemy func construct

--- server/routes.py
+++ server/routes.py
@@ -1,13 +1,18 @@
 from flask import render_template, request
-
 from models import Course
-
+from app import db
+from  sqlalchemy.sql.expression import func
 
 # avoiding import circulation 
 def register_routes(app,db):
+    print('check db:', db)
     
     @app.route('/')
-    def my_db():
-        course = Course.query.all()
-        return str(course)
+    def home():
         
+        course = Course.query.with_entities(Course.cid).order_by(func.random()).limit(10).all()
+        
+        
+        return render_template('index.html', course=course)
+        
+            
