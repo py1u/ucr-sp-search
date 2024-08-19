d3c799a15ad4295d94e4b2f2cdb47fe757903850
py1u.dev <py1u.dev@gmail.com>
2024-08-18 21:20:09 -0700

    create: server appliation for flask dev

--- server/app.py
+++ server/app.py
@@ -1,9 +1,4 @@
-import sqlite3
-import os
-from flask import Flask,  render_template
-from flask import g
-from flask import Flask, jsonify
-from markupsafe import escape
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -12,19 +7,14 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.db'
-    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
     
-    from routes import register_routes
-    register_routes(app,db)
+    with app.app_context():
+        from routes import register_routes
+        register_routes(app,db)
     
-    migrate = Migrate(app,db)
+    migrate = Migrate(app, db)
     
     return app
-
-    
-
-
-   
- 
